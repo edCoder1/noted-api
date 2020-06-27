682631f21e3e8af75a6f1d2a3ad4238c69a5db4b
edCoder1 <edwin.lsb1@gmail.com>
2020-06-26 21:08:13 -0500

    update and delete notebook endpoints, fix little issue with DB entity

--- src/main/java/noted/api/NotebookController.java
+++ src/main/java/noted/api/NotebookController.java
@@ -3,9 +3,16 @@ package noted.api;
 import noted.api.DTO.NotebookDTO;
 import noted.model.Notebook;
 import noted.services.NotebookService;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+// import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @CrossOrigin
 @RestController
@@ -37,4 +44,15 @@ public class NotebookController {
     public Notebook addNotes(@PathVariable String id) {
         return this._notebookService.addNotes(id);
     }
+
+    @RequestMapping(value = "/notebook", method = RequestMethod.PUT)
+    public Notebook updateNotebookName(@RequestBody Notebook notebook) {
+        return this._notebookService.updateNotebook(notebook);
+    }
+
+    @RequestMapping(value = "/notebook/{id}", method = RequestMethod.DELETE)
+    public void deleteNotebook(@PathVariable String id) {
+        this._notebookService.deleteNotebook(id);
+    }
+
 }
