8b170f03ee07038f7f84620a9e5c88ff0c1907d3
edCoder1 <edwin.lsb1@gmail.com>
2020-07-04 13:45:27 -0500

    MOcked Stuff with Mockito

--- src/test/java/com/noted/api/unit/services/NotebookServiceTests.java
+++ src/test/java/com/noted/api/unit/services/NotebookServiceTests.java
@@ -1,60 +1,39 @@
 package com.noted.api.unit.services;
 
-import com.noted.api.interfaces.INotebookService;
+import com.noted.api.db.NotebooksRepository;
 import com.noted.api.model.Notebook;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.noted.api.services.NotebookService;
 
-import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
 import java.util.List;
 
-class NotebookService_STUB implements INotebookService {
-
-    @Override
-    public List<Notebook> getAll() {
-        final List<Notebook> expected = new ArrayList<Notebook>();
-        expected.add(new Notebook("STUBBED NOTEBOOK!!!"));
-        return expected;
-    }
-
-    @Override
-    public Notebook getById(final String id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Notebook addNotes(final String id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Notebook createNotebook(final String name) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-}
-
+@ExtendWith(MockitoExtension.class)
 public class NotebookServiceTests {
 
-    // @InjectMocks
-    // NotebooksRepository _notebooksRepository;
+    @InjectMocks
+    private NotebookService service;
 
-    // @Mock
-    // NotebooksRepository get_notebooksRepository;
+    @Mock
+    private NotebooksRepository repository;
 
     @Test
     public void getAllNotebooks_BASIC() {
-        final INotebookService _notebookService = new NotebookService_STUB();
 
-        final List<Notebook> allNotebooks = _notebookService.getAll();
+        when(repository.findAll()).thenReturn(
+                Arrays.asList(new Notebook("MOCKED NOTEBOOK!!! 01"), new Notebook("MOCKED NOTEBOOK!!! 02")));
 
-        final List<Notebook> expected = new ArrayList<>();
-        expected.add(new Notebook("STUBBED NOTEBOOK!!!"));
+        final List<Notebook> actual = this.service.getAll();
 
-        assertEquals(expected.toArray().length, allNotebooks.toArray().length);
+        assertEquals("MOCKED NOTEBOOK!!! 02", actual.get(1).getName());
     }
 
 }
